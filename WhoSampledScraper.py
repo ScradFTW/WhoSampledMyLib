f0d7ad600931db3443a5304eeb5a8639b2e0ae50
Brad Jobe <scradftw@gmail.com>
2015-08-19 15:26:30 -0300

    fixed some formatting issues

--- WhoSampledScraper.py
+++ WhoSampledScraper.py
@@ -29,13 +29,17 @@ class WhoSampledScraper:
         songfile = eyed3.load(songLoc)
         self.artistName = songfile.tag.artist
         self.songTitle = songfile.tag.title
-        self.whoSampledURL = self.URL_WHOSAMPLED + "/" + self.artistName.replace(" ", "-") + "/" + self.songTitle.replace(" ", "-")
+        self.whoSampledURL = self.URL_WHOSAMPLED + \
+                             ("/" + self.artistName + "/" + \
+                             self.songTitle).replace(" ", "-")
 
     def getSongsSampled(self):
-        return self.sampleScraper(self.artistName, self.songTitle, "songsSampled")
+        return self.sampleScraper(self.artistName, self.songTitle, \
+                                  "songsSampled")
 
     def getWhoSampled(self):
-        return self.sampleScraper(self.artistName, self.songTitle, "whoSampled")
+        return self.sampleScraper(self.artistName, self.songTitle, \
+                                  "whoSampled")
 
     def sampleScraper(self, artistName, songTitle, calltype):
         whoSampledHTML = (urllib2.urlopen(self.whoSampledURL)).read()
@@ -44,10 +48,13 @@ class WhoSampledScraper:
         if calltype == "songsSampled":
             artistNamesFromSamples = whoSampledDoc.find_class("trackArtist")
             songTitlesFromSamples = whoSampledDoc.find_class("trackName")
-            if len(artistNamesFromSamples) != len(songTitlesFromSamples) or len(artistNamesFromSamples) < 1:
+            if len(artistNamesFromSamples) != len(songTitlesFromSamples) \
+                or len(artistNamesFromSamples) < 1:
                 return None
 
             for i in range(0, len(artistNamesFromSamples)):
-                self.sampleList.append(songTitlesFromSamples[i].text_content() + " " +  artistNamesFromSamples[i].text_content())
+                self.sampleList.append( \
+                        songTitlesFromSamples[i].text_content() + " " + \
+                        artistNamesFromSamples[i].text_content())
 
             return self.sampleList
